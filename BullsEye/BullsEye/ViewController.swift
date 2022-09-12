5701003fc1bce115f86a6299f8ad3730fe8a3701
Jia Jie Chan <jchan@sph.com.sg>
2022-09-12 14:23:02 +0800

    Functionalities

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -8,13 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+    
     @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentValue)"
+        
         let alert = UIAlertController(title: "Hello World", message: "This is my first app!", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Awesome", style: .default)
