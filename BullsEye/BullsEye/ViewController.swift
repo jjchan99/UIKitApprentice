d4a0e90c7e5af1b5c53814b253d6b67c0f1809aa
Jia Jie Chan <jchan@sph.com.sg>
2022-09-13 12:26:13 +0800

    Chapter 4. Outlets complete

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 class ViewController: UIViewController {
     
     var currentValue: Int = 0
+    var targetValue = Int.random(in: 1...100)
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
@@ -21,14 +26,25 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue)"
-        
-        let alert = UIAlertController(title: "Hello World", message: "This is my first app!", preferredStyle: .alert)
+        let message = "The value of the slider is: \(currentValue)" + "\nThe target is: \(targetValue)"
+        let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Awesome", style: .default)
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+    func updateLabels() {
+        targetLabel.text = "\(targetValue)"
     }
 
 
